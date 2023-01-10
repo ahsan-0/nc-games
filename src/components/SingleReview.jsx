0302b7e68883539673df37e4c74b4d5311adb1f2
ahsan-0 <ashanb995@gmail>
2023-01-10 11:22:47 +0000

    format time and reorder singleReview property and add isLoading state for more information

--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -1,20 +1,28 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getSingleReview } from "../api";
+import moment from "moment";
 
 function SingleReview() {
   const { review_id } = useParams();
   const [singleReview, setSingleReview] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    setIsLoading(true);
     getSingleReview(review_id).then(({ data }) => {
       setSingleReview(data.review);
+      setIsLoading(false);
     });
   }, [review_id]);
+  if (isLoading) {
+    return <p>is Loading...</p>;
+  }
   return (
     <div key={singleReview.review_id}>
-      <li>Review title: {singleReview.title}</li>
-      <p>Owner: {singleReview.owner}</p>
-      <p>Created at: {singleReview.created_at}</p>
+      <li>Owner: {singleReview.owner}</li>
+      <p>Review title: {singleReview.title}</p>
+      <p>Content: {singleReview.review_body}</p>
+      <p>Created at: {moment(singleReview.created_at).format("DD-MM-YYYY")}</p>
     </div>
   );
 }
