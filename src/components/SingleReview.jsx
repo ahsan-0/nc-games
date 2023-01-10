ceecd6d869810d21c8f18d0c2adbe72b76c5bd5f
ahsan-0 <ashanb995@gmail>
2023-01-10 15:14:19 +0000

    display comments on the same page as the single review

--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
-import { getSingleReview } from "../api";
+import { useParams } from "react-router-dom";
+import { getComments, getSingleReview } from "../api";
 import moment from "moment";
 
 function SingleReview() {
   const { review_id } = useParams();
   const [singleReview, setSingleReview] = useState([]);
+  const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     setIsLoading(true);
@@ -13,21 +14,33 @@ function SingleReview() {
       setSingleReview(data.review);
       setIsLoading(false);
     });
+    getComments(review_id).then(({ data }) => {
+      setComments(data.comments);
+    });
   }, [review_id]);
   if (isLoading) {
     return <p>is Loading...</p>;
   }
   return (
     <div key={singleReview.review_id}>
+      <h3>Review</h3>
       <li>Owner: {singleReview.owner}</li>
       <p>Review title: {singleReview.title}</p>
       <p>Category: {singleReview.category}</p>
       <p>Content: {singleReview.review_body}</p>
       <p>Votes: {singleReview.votes}</p>
       <p>Created at: {moment(singleReview.created_at).format("DD-MM-YYYY")}</p>
-      <p>
-        Click <Link to={`/review/${singleReview.review_id}/comments`}>here</Link> for comments to {singleReview.owner}
-      </p>
+      {comments.length === 0 ? <p>No comments available</p> : <h3>Comments:</h3>}
+      {comments.map((comment) => {
+        return (
+          <div key={comment.comment_id}>
+            <li>Author: {comment.author}</li>
+            <p>Comment: {comment.body}</p>
+            <p>Votes: {comment.votes}</p>
+            <p>Created at: {moment(comment.created_at).format("DD-MM-YYYY")}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
