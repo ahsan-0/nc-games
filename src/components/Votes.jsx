fefc1ac7ed50af641dd759e9071d016ef80cdcf7
ahsan-0 <ashanb995@gmail>
2023-01-12 16:56:33 +0000

    fix display format

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -4,7 +4,7 @@ function Votes({ voteData }) {
   const [upVote, setUpVote] = useState(0);
   return (
     <div>
-      <p>Votes:{voteData + upVote}</p>
+      <p>Votes: {voteData + upVote}</p>
       <button
         onClick={() => {
           setUpVote((curr) => (curr += 1));
