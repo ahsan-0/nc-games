c53ea8114bf37a469ce66f3097d006a1d1a66df1
ahsan-0 <ashanb995@gmail>
2023-01-12 16:28:27 +0000

    add className to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav>
+    <nav className="nav">
       <Link to="/categories">Game-Categories</Link>
       <Link to="/reviews">Reviews</Link>
       <Link to="/users">Users</Link>
