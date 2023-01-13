16df20bcb4a32a075c37bb5dd1e87177e034a108
ahsan-0 <ashanb995@gmail>
2023-01-13 15:45:00 +0000

    optimistically render review votes & add error useState to display information when error occurs

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import { patchCommentVotes } from "../api";
 
-function Votes({ singleReviewVotes, commentVoteData, id, setVotes }) {
+function Votes({ singleReviewVotes, commentVoteData, setVotes, id }) {
   const [commentVotes, setCommentVotes] = useState(commentVoteData);
   const [upVote, setUpVote] = useState(1);
+  const [error, setError] = useState(null);
   const patchData = {
     inc_votes: 1,
   };
+  if (error) {
+    return <h4>Error could not update votes</h4>;
+  }
   return (
     <div>
-      {commentVotes ? <p>Votes: {commentVotes}</p> : <p>Votes: {singleReviewVotes}</p>}
+      <p>Votes: {commentVotes ? commentVotes : singleReviewVotes}</p>
       <button
         id={commentVotes ? commentVotes : singleReviewVotes}
         onClick={(event) => {
           setUpVote((curr) => curr + 1);
+          setVotes(singleReviewVotes + 1);
           if (parseInt(event.target.id) === singleReviewVotes) {
-            patchCommentVotes(id, patchData).then(() => {
-              setVotes(singleReviewVotes + 1);
-            });
+            patchCommentVotes(id, patchData)
+              .then(() => {})
+              .catch((err) => {
+                setError(err);
+              });
           }
           setCommentVotes(commentVoteData + upVote);
         }}
